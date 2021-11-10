93fef184008be1b2001e8f9090a345b0c10e4d6f
srostron2112 <srostron@usca.edu>
2021-11-10 13:11:20 -0500

    1 file modified

--- js/hotel-json.js
+++ js/hotel-json.js
@@ -1,40 +1,40 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCnTiONA5kJi8Hroxd3dimv-AlRHeLnpOM",
-  authDomain: "csci225-bab55.firebaseapp.com",
-  projectId: "csci225-bab55",
-  storageBucket: "csci225-bab55.appspot.com",
-  messagingSenderId: "145918814164",
-  appId: "1:145918814164:web:8bdf18387236af79699841",
-  measurementId: "G-CRJL29Q68X"
+  apiKey: 'AIzaSyCnTiONA5kJi8Hroxd3dimv-AlRHeLnpOM',
+  authDomain: 'csci225-bab55.firebaseapp.com',
+  projectId: 'csci225-bab55',
+  storageBucket: 'csci225-bab55.appspot.com',
+  messagingSenderId: '145918814164',
+  appId: '1:145918814164:web:8bdf18387236af79699841',
+  measurementId: 'G-CRJL29Q68X',
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// object examples 
+// object examples
 var testJson = {};
-testJson["lastname"] = "zhang";
-testJson["location"] = "aiken";
+testJson['lastname'] = 'zhang';
+testJson['location'] = 'aiken';
 console.log(testJson);
 
 // enter data in
-$("input[type='button']").click(function(e) {
+$("input[type='button']").click(function (e) {
   //get the value of form
   const inputdata = $('form').serializeArray();
   console.log(inputdata);
-  
+
   /* save the data to database */
-  var inputJson={};
-  for(var i = 0; i<inputdata.length; i++){
-    var n = inputdata[i]["name"];
-    var v = inputdata[i]["value"];
+  var inputJson = {};
+  for (var i = 0; i < inputdata.length; i++) {
+    var n = inputdata[i]['name'];
+    var v = inputdata[i]['value'];
+    console.log(n + ' ' + v);
     inputJson[n] = v;
   }
-    
-    firebase.firestore().collection('hotelreservation').add(inputJson);
 
+  firebase.firestore().collection('hotelreservation').add(inputJson);
 
   /* clear the entry */
-  $("form")[0].reset();
+  $('form')[0].reset();
 });
 
 /* array example
